c7c2b7d4bd5c75888b69d26c7b211fba908d4596
TUCHAGUES <axel.tuchagues@orange.fr>
2024-03-26 17:17:38 -0400

    Modification pour ajouter Jetpack Compose sur les 2 pages

--- app/src/main/java/com/tunajam/app/home/HomeActivity.kt
+++ app/src/main/java/com/tunajam/app/home/HomeActivity.kt
@@ -5,7 +5,14 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -14,9 +21,18 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.tunajam.app.R
+import com.tunajam.app.model.TunaJamPhoto
 import com.tunajam.app.spotify_login.SpotifyAPI
+import com.tunajam.app.ui.screens.TunaJamUiState
 import com.tunajam.app.ui.theme.TunaJamTheme
 import com.tunajam.app.user_data.UserData
 
@@ -53,7 +69,9 @@ class HomeActivity : ComponentActivity(){
                                 }
                                 setContent {
                                     TunaJamTheme {
-                                        UserInfo(user = user)
+                                        val mockData = List(10) { TunaJamPhoto("$it", "") }
+                                        val tunaJamUiState = TunaJamUiState.Success(mockData)
+                                        HomeScreen(tunaJamUiState = tunaJamUiState, retryAction = { /*TODO*/ })
                                     }
                                 }
                             }
@@ -94,11 +112,141 @@ fun UserInfo(user:User){
 }
 
 
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        TunaJamTheme {
-            UserInfo(user = User("John Doe"))
+@Composable
+fun HomeScreen(
+    tunaJamUiState: TunaJamUiState, retryAction: () -> Unit, modifier: Modifier = Modifier, contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    when (tunaJamUiState) {
+        is TunaJamUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is TunaJamUiState.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
+        //Load playlist pictures
+        is TunaJamUiState.Success -> PhotosGridScreen(tunaJamUiState.photos, modifier)
+
+        else -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+/**
+ * The home screen displaying the loading message.
+ */
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+/**
+ * The home screen displaying error message with re-attempt button.
+ */
+@Composable
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+/**
+ * ResultScreen displaying number of photos retrieved.
+ */
+@Composable
+fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Text(text = photos)
+    }
+}
+
+@Composable
+fun TunaJamPhotoCard(photo: TunaJamPhoto, modifier: Modifier = Modifier) {
+
+    //formate les images format carré
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(photo.imgSrc)
+                .crossfade(true)
+                .build(),
+            error = painterResource(R.drawable.ic_broken_image),
+            placeholder = painterResource(R.drawable.loading_img),
+            contentDescription = stringResource(R.string.tunaJam_photo),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+//charge les photos de playliste sous forme de grille
+fun PhotosGridScreen(
+    photos: List<TunaJamPhoto>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding,
+    ) {
+        items(items = photos, key = { photo -> photo.id }) { photo ->
+            TunaJamPhotoCard(
+                photo,
+                modifier = modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingScreenPreview() {
+    TunaJamTheme {
+        LoadingScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorScreenPreview() {
+    TunaJamTheme {
+        ErrorScreen({})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhotosGridScreenPreview() {
+    TunaJamTheme {
+        val mockData = List(10) { TunaJamPhoto("$it", "") }
+        PhotosGridScreen(mockData)
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TunaJamTheme {
+      UserInfo(user = User("John Doe"))
         }
     }
 }
--- app/src/main/java/com/tunajam/app/spotify_login/SpotifyLoginActivity.kt
+++ app/src/main/java/com/tunajam/app/spotify_login/SpotifyLoginActivity.kt
@@ -2,14 +2,25 @@ package com.tunajam.app.spotify_login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.tunajam.app.R
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
 import com.tunajam.app.home.HomeActivity
+import com.tunajam.app.spotify_login.MainActivity.Companion.authenticateWithSpotify
 import com.tunajam.app.user_data.UserData
 
 
@@ -18,14 +29,11 @@ const val REDIRECT_URI = "com.tunajam.app://callback"
 const val REQUEST_CODE = 1337 // On peut mettre n'importe quel nombre ici
 
 class MainActivity : ComponentActivity() {
-    private lateinit var btnSpot : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnSpot = findViewById(R.id.connectButton)
-        btnSpot.setOnClickListener {
-            authenticateWithSpotify()
+        setContent {
+            LoginScreen(this@MainActivity)
         }
     }
 
@@ -35,20 +43,6 @@ class MainActivity : ComponentActivity() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
         finish()
-    }
-
-    private fun authenticateWithSpotify() {
-        val builder = AuthorizationRequest.Builder(
-            CLIENT_ID,
-            AuthorizationResponse.Type.CODE,
-            REDIRECT_URI // L'URI de redirection après l'authentification
-        )
-        // Add offline access to get a refresh token
-        builder.setShowDialog(true) // Afficher une boîte de dialogue pour l'authentification
-        builder.setScopes(arrayOf("user-read-email", "user-library-read", "user-library-modify", "user-top-read",
-            "playlist-modify-public","playlist-modify-private")) // Les permissions que l'on demande à l'utilisateur
-        val request = builder.build() // On construit la requête d'authentification
-        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request) // On ouvre l'activité d'authentification
     }
 
     @Deprecated("Deprecated in Java")
@@ -81,5 +75,41 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    companion object {
+        fun authenticateWithSpotify(mainActivity: MainActivity) {
+            val builder = AuthorizationRequest.Builder(
+                CLIENT_ID,
+                AuthorizationResponse.Type.CODE,
+                REDIRECT_URI // L'URI de redirection après l'authentification
+            )
+            // Add offline access to get a refresh token
+            builder.setShowDialog(true) // Afficher une boîte de dialogue pour l'authentification
+            builder.setScopes(arrayOf("user-read-email", "user-library-read", "user-library-modify", "user-top-read",
+                "playlist-modify-public","playlist-modify-private")) // Les permissions que l'on demande à l'utilisateur
+            val request = builder.build() // On construit la requête d'authentification
+            AuthorizationClient.openLoginActivity(mainActivity, REQUEST_CODE, request) // On ouvre l'activité d'authentification
+        }
+    }
 }
 
+
+@Composable
+fun LoginScreen(mainActivity: MainActivity) {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { authenticateWithSpotify(mainActivity) },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Connexion avec Spotify")
+            }
+        }
+    }
+}
